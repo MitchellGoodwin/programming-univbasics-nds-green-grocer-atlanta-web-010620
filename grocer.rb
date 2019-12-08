798a9f9de623e1e99c1d602c67f0e9f23c3e29b4
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-08 00:15:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
   index = 0 
   while index < coupons.length do 
-    
+    item_coupon_name
 end
 
 def apply_clearance(cart)
