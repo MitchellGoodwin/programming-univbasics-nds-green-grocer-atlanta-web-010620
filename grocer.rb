1c8419aeed9246be5fabfc0bf75deecd8c03f2d0
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-08 00:06:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def consolidate_cart(cart)
     if new_cart_item != nil
       new_cart_item[:count] += 1
     else
-      new_cart_item = 
+      new_cart_item = {
       consolidated_cart << new_cart_item
     end
     index += 1 
