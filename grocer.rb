c77c8ecde8d6240180a53a3ecb19c94835202b5d
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-08 00:18:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,6 +45,7 @@ def apply_coupons(cart, coupons)
   while index < coupons.length do 
     item_coupon_name = "#{coupons[index][:item]} W/COUPON"
     cart_item = find_item_by_name_in_collection(coupons[index][:item], cart]
+    
 end
 
 def apply_clearance(cart)
