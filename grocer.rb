fbb9466f184cf92aabca264f0ea1fb087b24fe1e
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-08 00:01:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def consolidate_cart(cart)
   while index < cart.length do 
     new_cart_item = find_item_by_name_in_collection(cart[index][:item], consolidated_cart)
     if new_cart_item != nil
-       
+      new_cart_item[] 
     else
       consolidated_cart << cart[index]
     end
